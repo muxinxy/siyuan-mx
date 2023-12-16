d18f447d913701749dddbc348a3931444272fc00
muxinxy <zeng5360@gmail.com>
2023-12-16 21:48:23 +0800

    Comment u

--- kernel/model/conf.go
+++ kernel/model/conf.go
@@ -761,8 +761,8 @@ func InitBoxes() {
 }
 
 func IsSubscriber() bool {
-	u := Conf.GetUser()
 	return true
+	// u := Conf.GetUser()
 	// return nil != u && (-1 == u.UserSiYuanProExpireTime || 0 < u.UserSiYuanProExpireTime) && 0 == u.UserSiYuanSubscriptionStatus
 }
 
